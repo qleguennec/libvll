1e48b15c23ddd437b2391fd425c96764c1e7541e
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-28 01:09:38 +0000

    fix vll_free

--- libvll.h
+++ libvll.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 21:32:51 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/27 21:14:51 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/28 00:46:40 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,7 @@ t_vll_node				*vll_tail(t_vll *l);
 t_vll_node				*vll_tail_add(t_vll *l, void *data, size_t size);
 t_vll_node				*vll_vect_node_new(void *data, size_t size);
 void					vll_dup(t_vll *dest, t_vll *src);
+void					vll_free(t_vll *l);
 void					vll_init(t_vll *l);
 void					vll_node_add_back(t_vll *l, t_vll_node *n, t_vll_node *new);
 void					vll_node_add_front(t_vll *l, t_vll_node *n, t_vll_node *new);
---
+++ vll_free.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vll_free.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/28 00:40:04 by qle-guen          #+#    #+#             */
+/*   Updated: 2016/11/28 01:09:35 by qle-guen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libvll.h"
+
+static void	free_elems(t_vll_node *n)
+{
+	t_vll_node	*m;
+
+	while (n)
+	{
+		vect_del(n->p);
+		m = n->next;
+		free(n);
+		n = m;
+	}
+}
+
+void		vll_free(t_vll *l)
+{
+	t_vll_node	*n;
+	t_vll_node	*m;
+
+	n = l->head;
+	if (!n)
+	{
+		free(l);
+		return ;
+	}
+#if (VLL_VLL)
+	if (IS_VLL(l))
+	{
+		while (n)
+		{
+			vll_free(n->p);
+			m = n->next;
+			free(n);
+			n = m;
+		}
+	}
+#endif
+	free_elems(n);
+	free(l);
+}
